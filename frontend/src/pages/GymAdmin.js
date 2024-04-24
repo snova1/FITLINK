038fb0d6630db7160776824885084602f9311f2a
snova1 <snova@unal.edu.co>
2024-04-23 19:31:23 -0500

    Añadir imágenes de gym

--- frontend/src/pages/GymAdmin.js
+++ frontend/src/pages/GymAdmin.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../hojas-de-estilo/GymAdmin.css';
 import Footer from '../Footer';
@@ -7,6 +7,16 @@ function GymAdmin() {
   const navigate = useNavigate();
   const handleModifyInfoClick = () => {
     navigate('/GymModify'); 
+  };
+  const [selectedImages, setSelectedImages] = useState([]);
+  const handleImageUpload = (event) => {
+    const files = event.target.files;
+    const newImages = Array.from(files);
+    setSelectedImages((prevImages) => [...prevImages, ...newImages]);
+  };
+
+  const handleDeleteImage = (index) => {
+    setSelectedImages((prevImages) => prevImages.filter((_, i) => i !== index));
   };
   return (
     <div className="gym-admin-dashboard">
@@ -36,12 +46,21 @@ function GymAdmin() {
         </div>
       </div>
       <div className="image-scroll">
-        {/* Scroll de imágenes */}
-        <button className="add-image-button">Agregar foto</button>
+          {/* Scroll de imágenes */}
+          {selectedImages.map((image, index) => (
+            <div key={index} className="image-container">
+              <img src={URL.createObjectURL(image)} alt={`Uploaded ${index}`} className="uploaded-image" />
+              <button className="delete-image-button" onClick={() => handleDeleteImage(index)}>x</button>
+            </div>
+          ))}
+      </div>
+      <div className="add-image-container">
+          <label htmlFor="upload-button" className="add-image-button">Agregar foto</label>
+          <input type="file" id="upload-button" accept="image/*" multiple onChange={handleImageUpload} style={{ display: 'none' }} />
       </div>
       <div className="machine-scroll">
         {/* Scroll de máquinas */}
-        <button className="add-image-button">Agregar máquinas</button>
+        <button className="add-machine-button">Agregar máquinas</button>
       </div>
       <div className="schedule">
         <h2 className="schedule-title">Tu horario de atención</h2>
